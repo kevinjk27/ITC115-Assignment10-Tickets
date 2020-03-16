76371d228bb976c89d090f95878cd417d49b039d
Kevin Kantono <52363107+kevinjk27@users.noreply.github.com>
2020-03-16 16:20:26 -0700

    Add files via upload

--- StudentAdvanceTickets.java
+++ StudentAdvanceTickets.java
@@ -4,7 +4,15 @@ public class StudentAdvanceTickets extends AdvanceTickets {
 
 	public StudentAdvanceTickets(int daysBeforeEvent) {
 		super(daysBeforeEvent);
-		// TODO Auto-generated constructor stub
+		this.daysBeforeEvent = daysBeforeEvent;
+		if (daysBeforeEvent >= 10) {
+			setTicketPrice((getTicketPrice() / 2.0));
+		} else if (daysBeforeEvent < 10 || daysBeforeEvent > 0) {
+			setTicketPrice((getTicketPrice() / 2.0));
+		}
 	}
 
+	public String toString() {
+		return "Student Advance Category Ticket number: " + ticketNumber + ", Price: $" + super.getTicketPrice();
+	}
 }
--- TicketsClient.java
+++ TicketsClient.java
@@ -5,12 +5,13 @@ public class TicketsClient {
 	public static void main(String[] args) {
 		WalkupTickets ticket1 = new WalkupTickets();
 		WalkupTickets ticket2 = new WalkupTickets();
-		WalkupTickets ticket3 = new WalkupTickets();
-		AdvanceTickets ticket4 = new AdvanceTickets(-5);
+		AdvanceTickets ticket4 = new AdvanceTickets(5);
+		StudentAdvanceTickets ticket5 = new StudentAdvanceTickets(5);
 		System.out.println(ticket1.toString());
 		System.out.println(ticket2.toString());
-		System.out.println(ticket3.toString());
 		System.out.println(ticket4.toString());
+		System.out.println(ticket5.toString());
+		
 
 	}
 
